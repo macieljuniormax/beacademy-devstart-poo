5a1d85e9e6018e3b2c29ebaa25cd97afda497b17
Maciel Júnior <macielfcjunior@icloud.com>
2022-06-11 15:36:24 -0300

    feat: Coloca namespaces nas classes

---
+++ Aula 06 - Composer/index.php
@@ -0,0 +1,8 @@
+<?php
+
+include 'Classes/Usuario.php';
+include 'Classes/Cliente.php';
+include 'Classes/Gestor.php';
+include 'Classes/Categoria.php';
+include 'Classes/Produto.php';
+include 'Classes/Config/Usuario.php';
